df88ced11451782bd9d8f67995c14db1f3885d7d
iori tobeta <15eb1137@gmail.com>
2022-10-01 10:49:18 +0900

    gaugeの軸の詳細化

--- flutter/lib/components/gauge_chart.dart
+++ flutter/lib/components/gauge_chart.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
@@ -18,30 +20,132 @@ class GaugeChart extends ConsumerWidget {
             ? himawari.withSaturation(0.60).toColor()
             : himawari.withSaturation(0.20).toColor();
     final colorSub = Colors.blueGrey.shade100;
-    return SfRadialGauge(axes: <RadialAxis>[
-      RadialAxis(
-          minimum: 0,
-          maximum: 500,
-          showAxisLine: false,
-          showLabels: false,
-          ranges: <GaugeRange>[
-            GaugeRange(startValue: 0, endValue: brightness, color: colorMain),
-            GaugeRange(startValue: brightness, endValue: 500, color: colorSub)
-          ],
-          annotations: <GaugeAnnotation>[
-            GaugeAnnotation(
-                widget: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    textBaseline: TextBaseline.alphabetic,
-                    crossAxisAlignment: CrossAxisAlignment.baseline,
-                    children: [
-                  Text('${brightness.floor()}',
-                      style:
-                          TextStyle(fontSize: 64, color: himawari.toColor())),
-                  const Text('lx',
-                      style: TextStyle(fontSize: 24, color: Colors.blueGrey))
-                ]))
-          ])
-    ]);
+    double logit(num x) =>
+        (log(x / (1500 - x)) / log(1.11)) +
+        50; //1 / (1 + pow(1.07, -x + 100 / 2));
+    final List<RadialAxis> axes = brightness > 1
+        ? <RadialAxis>[
+            RadialAxis(
+                radiusFactor: 0.9,
+                minimum: 0,
+                maximum: logit(1200),
+                showAxisLine: false,
+                showLabels: false,
+                ranges: <GaugeRange>[
+                  GaugeRange(
+                    startValue: 0,
+                    endValue: logit(brightness),
+                    color: colorMain,
+                    startWidth: 0.3,
+                    endWidth: 0.3,
+                    sizeUnit: GaugeSizeUnit.factor,
+                  ),
+                  GaugeRange(
+                    startValue: logit(brightness),
+                    endValue: logit(1200),
+                    color: colorSub,
+                    startWidth: 0.3,
+                    endWidth: 0.3,
+                    sizeUnit: GaugeSizeUnit.factor,
+                  )
+                ],
+                pointers: <GaugePointer>[
+                  MarkerPointer(
+                      markerType: MarkerType.text,
+                      value: logit(20),
+                      text: '睡眠',
+                      textStyle: const GaugeTextStyle(color: Colors.grey),
+                      offsetUnit: GaugeSizeUnit.factor,
+                      markerOffset: -0.1),
+                  MarkerPointer(
+                      markerType: MarkerType.invertedTriangle,
+                      value: logit(20),
+                      color: Colors.deepOrange),
+                  MarkerPointer(
+                      markerType: MarkerType.text,
+                      value: logit(75),
+                      text: 'トイレ',
+                      textStyle: const GaugeTextStyle(color: Colors.grey),
+                      offsetUnit: GaugeSizeUnit.factor,
+                      markerOffset: -0.1),
+                  MarkerPointer(
+                      markerType: MarkerType.invertedTriangle,
+                      value: logit(75),
+                      color: Colors.deepOrange),
+                  MarkerPointer(
+                      markerType: MarkerType.text,
+                      value: logit(200),
+                      text: 'テレビ',
+                      textStyle: const GaugeTextStyle(color: Colors.grey),
+                      offsetUnit: GaugeSizeUnit.factor,
+                      markerOffset: -0.1),
+                  MarkerPointer(
+                      markerType: MarkerType.invertedTriangle,
+                      value: logit(200),
+                      color: Colors.deepOrange),
+                  MarkerPointer(
+                      markerType: MarkerType.text,
+                      value: logit(300),
+                      text: '料理',
+                      textStyle: const GaugeTextStyle(color: Colors.grey),
+                      offsetUnit: GaugeSizeUnit.factor,
+                      markerOffset: -0.1),
+                  MarkerPointer(
+                      markerType: MarkerType.invertedTriangle,
+                      value: logit(300),
+                      color: Colors.deepOrange),
+                  MarkerPointer(
+                      markerType: MarkerType.text,
+                      value: logit(400),
+                      text: 'PC',
+                      textStyle: const GaugeTextStyle(color: Colors.grey),
+                      offsetUnit: GaugeSizeUnit.factor,
+                      markerOffset: -0.1),
+                  MarkerPointer(
+                      markerType: MarkerType.invertedTriangle,
+                      value: logit(400),
+                      color: Colors.deepOrange),
+                  MarkerPointer(
+                      markerType: MarkerType.text,
+                      value: logit(700),
+                      text: '字書き',
+                      textStyle: const GaugeTextStyle(color: Colors.grey),
+                      offsetUnit: GaugeSizeUnit.factor,
+                      markerOffset: -0.1),
+                  MarkerPointer(
+                      markerType: MarkerType.invertedTriangle,
+                      value: logit(700),
+                      color: Colors.deepOrange),
+                  MarkerPointer(
+                      markerType: MarkerType.text,
+                      value: logit(1000),
+                      text: '裁縫',
+                      textStyle: const GaugeTextStyle(color: Colors.grey),
+                      offsetUnit: GaugeSizeUnit.factor,
+                      markerOffset: -0.1),
+                  MarkerPointer(
+                      markerType: MarkerType.invertedTriangle,
+                      value: logit(1000),
+                      color: Colors.deepOrange)
+                ],
+                annotations: <GaugeAnnotation>[
+                  GaugeAnnotation(
+                      widget: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          textBaseline: TextBaseline.alphabetic,
+                          crossAxisAlignment: CrossAxisAlignment.baseline,
+                          children: [
+                        Text('${brightness.floor()}',
+                            style: TextStyle(
+                                fontSize: 64, color: himawari.toColor())),
+                        const Text('lx',
+                            style:
+                                TextStyle(fontSize: 24, color: Colors.blueGrey))
+                      ]))
+                ])
+          ]
+        : [];
+
+    return SfRadialGauge(axes: axes);
   }
 }
